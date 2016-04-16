8537c59be71cd27e3bf3907e9494f64c64fb7644
Max Fowler <maxhfowler@gmail.com>
2016-04-16 00:33:09 -0400

    polishing

--- hello_utilities/text_helper.py
+++ hello_utilities/text_helper.py
@@ -1,26 +1,29 @@
-from hello_settings import SECRETS_DICT
-
 import smtplib
 from email.mime.text import MIMEText
 from email.utils import formatdate
 
+from hello_settings import SECRETS_DICT
+
 
 def send_text(msg, to_phone_number):
-
+    """
+    helper for sending SMS text messages
+    :param msg: string text message to send
+    :param to_phone_number: string of phone number to send text to (must have provider in string)
+    :return:
+    """
     server = smtplib.SMTP("smtp.gmail.com", 587)
     server.starttls()
-    server.login('myfreehoroscopetoday@gmail.com', SECRETS_DICT['TEXT_SECRET'])
+    server.login(SECRETS_DICT['TEXT_EMAIL'], SECRETS_DICT['TEXT_PASSWORD'])
 
     msg = msg.encode('ascii', 'ignore')
     message = MIMEText(msg)
     message['Date'] = formatdate()
-    message['From'] = 'myfreehoroscopetoday@gmail.com'
+    message['From'] = SECRETS_DICT['TEXT_EMAIL']
 
-    server.sendmail('myfreehoroscopetoday@gmail.com', to_phone_number, message.as_string())
+    server.sendmail(SECRETS_DICT['TEXT_EMAIL'], to_phone_number, message.as_string())
     server.quit()
 
 
 if __name__ == '__main__':
-    # send_text('hello cman: colons bork it :) http://reddit.com', to_phone_number=SECRETS_DICT['MY_PHONE_NUMBER'])
     send_text('hello', to_phone_number=SECRETS_DICT['MY_PHONE_NUMBER'])
-    # send_text('hello', to_phone_number='maxhfowler@gmail.com')
